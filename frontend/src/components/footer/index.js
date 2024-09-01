9f8c655fc15d7133ebba94f6ce4d5e77af446b01
Roshan Dash <83511412+dashroshan@users.noreply.github.com>
2024-09-01 20:48:45 +0530

    Footer easter egg removed

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,12 +1,12 @@
 import classes from "./index.module.css";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function Footer(props) {
-    const [text, setText] = useState(0);
+    // const [text, setText] = useState(0);
     return (
         <footer className={classes.footer}>
-            <div onClick={() => setText(text + 1)}>
-                {text > 2 ? "Roshan" : "Made with"} ❤️ {text > 2 ? "Shreeya" : "by team Geekoquad"}
+            <div>
+                Made with ❤️ by Roshan Dash
             </div>
         </footer>
     );
